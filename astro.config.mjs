6f3339faff14d2f0204b194810ca195602f20349
Karl Ostendorf <karl@ostendorf.com>
2022-10-12 20:19:39 +0200

    add basic card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: 'https://example.com',
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
+  server: {
+    host: 'localhost'
+  },
+  integrations: [tailwind(), react()]
+});
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import '@tremor/react/dist/esm/tremor.css';
+import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
+
+export default () => (
+  <Card maxWidth="max-w-sm">
+    <Text>Sales</Text>
+    <Metric>$ 71,465</Metric>
+    <Flex marginTop="mt-4">
+      <Text>32% of annual target</Text>
+      <Text>$ 225,000</Text>
+    </Flex>
+    <ProgressBar percentageValue={32} marginTop="mt-2" />
+  </Card>
+);
